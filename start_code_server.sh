b740e0a80616b3d346b361e77a533bdb5122ca56
Radzim Sendyka <57621493+Radzim@users.noreply.github.com>
2025-10-20 15:24:16 +0100

    Update start_code_server.sh

--- start_code_server.sh
+++ start_code_server.sh
@@ -9,7 +9,8 @@ rm -rf /var/lib/apt/lists/*
 # --- Install code-server ---
 curl -fsSL https://code-server.dev/install.sh | sh
 
-# --- Start code-server ---
+# --- Start code-server in /workspace/runpod_data_test/data ---
+cd /workspace/runpod_data_test/data
 nohup /usr/bin/code-server \
   --bind-addr 0.0.0.0:8080 \
   --auth none \
