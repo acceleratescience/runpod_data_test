e28bbd4365ebba5f45c5317c416a841497353f29
Radzim Sendyka <57621493+Radzim@users.noreply.github.com>
2025-10-21 17:32:19 +0100

    Update clone_repo.sh to install Python packages
    Added Python package installation and cleanup steps.

--- clone_repo.sh
+++ clone_repo.sh
@@ -2,7 +2,7 @@
 set -e
 
 apt-get update -y
-apt-get install -y --no-install-recommends git ca-certificates
+apt-get install -y --no-install-recommends git python3-pip ca-certificates
 rm -rf /var/lib/apt/lists/*
 
 if [ -d /workspace/workshop/.git ]; then
@@ -11,3 +11,16 @@ else
   git clone https://github.com/acceleratescience/diffusion-models.git /workspace/workshop
   echo "Repository cloned to /workspace/workshop"
 fi
+
+cd /workspace/workshop
+
+# Remove unneeded files
+rm -rf .github docs overrides .dockerignore .pre-commit-config.yaml mkdocs.yml
+echo "Removed extra files."
+
+# Install dependencies using uv
+pip install uv
+uv sync
+
+# Extras
+pip install torch torchvision
